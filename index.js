908a4133efb0ef466b62254e1c60df945d23f112
ulmaskulov-ilya <147428607+ulmaskulov-ilya@users.noreply.github.com>
2025-03-04 19:19:47 +0500

    Update index.js

--- index.js
+++ index.js
@@ -11,11 +11,122 @@ function getFiles() {
     return filePaths.map(path => readFile(path));
 }
 
+function getTODO() {
+    let a = [];
+    for (let file of files) {
+        s = file.split("\n")
+        for (const line of s)
+        {
+            let regex = /\/\/\sTODO\s(.+)/g;
+            let match;
+            while ((match = regex.exec(line)) !== null) {
+                a.push(match[0]);
+            }
+        }
+    } 
+    return a;
+}
+
 function processCommand(command) {
-    switch (command) {
+    let c = command.split(" ")
+    a = getTODO();
+    switch (c[0]) {
         case 'exit':
             process.exit(0);
             break;
+        case 'show':
+            for (const line of a)
+            {
+                console.log(line);
+            }
+            break;
+        case 'user':
+            let name = c[1];
+            for (const line of a)
+                {
+                    if (line.toLowerCase().includes(name.toLowerCase()))
+                        console.log(line);
+                }
+            break;
+        case 'sort':
+            switch (c[1]) {
+                case 'importance':
+                    let dict = {}
+                    for (const line of a)
+                        {
+                            let cnt = (line.match(/!/g) || []).length;
+                            dict[cnt] = line;
+                        }
+                    const sortedKeys = Object.keys(dict).sort();
+                    const sortedDict = {};
+                    sortedKeys.forEach(key => {
+                        sortedDict[key] = dict[key];
+                    });
+                    let values = Object.values(sortedDict).reverse()
+                    for (const str of values)
+                    {
+                        console.log(str);
+                    }
+                    break;
+                case 'user':
+                    let dictWithName = {}
+                    let withoutNames = []
+                    for (const line of a)
+                        {
+                            let nameIndex = line.indexOf(";");
+                            if (nameIndex != -1)
+                            {
+                                if (!Object.keys(dictWithName).includes(line.slice(9, nameIndex).toLowerCase()))
+                                    dictWithName[line.slice(9, nameIndex).toLowerCase()] = [];
+                                dictWithName[line.slice(9, nameIndex).toLowerCase()].push(line);
+                            }
+                            else
+                                withoutNames.push(line);
+                        }
+                    let valuesNames = Object.values(dictWithName)
+                    for (const str of valuesNames)
+                    {
+                        for (const data of str)
+                            console.log(data);
+                    }
+                    for (const str of withoutNames)
+                        {
+                            console.log(str);
+                        }
+                    break;
+        
+                case 'date':
+                    let dictWithdate = {}
+                    let withoutDates = []
+                    for (const line of a)
+                        {
+                            let nameIndex = line.indexOf(";");
+                            let dateIndex = line.indexOf(";", nameIndex + 1);
+                            if (nameIndex != -1)
+                            {
+                                dictWithdate[line.slice(nameIndex + 2, dateIndex)] = line;
+                            }
+                            else
+                            withoutDates.push(line);
+                        }
+                    const sortedKeysDates = Object.keys(dictWithdate).sort((a, b) => new Date(b) - new Date(a));
+                    const sortedDictDates = {};
+                    sortedKeysDates.forEach(key => {
+                        sortedDictDates[key] = dictWithdate[key];
+                    });
+                    let v = Object.values(sortedDictDates)
+                    for (const str of v)
+                    {
+                        console.log(str)
+                    }
+                    for (const str of withoutDates)
+                        {
+                            console.log(str);
+                        }
+                    break;
+            }
+            break;
+
         default:
             console.log('wrong command');
             break;
